e5d0acbbb7058431e3ba47f6340247e09d38a61e
Grigoriy Zavodov <zavodov@gmail.com>
2020-02-08 20:16:03 +0200

    banner-rotation: UCB1 test

--- banner-rotation/algorithm/ucb1_test.go
+++ banner-rotation/algorithm/ucb1_test.go
@@ -2,7 +2,9 @@ package algorithm
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestUCB1Coverage(t *testing.T) {
@@ -34,6 +36,64 @@ func TestUCB1Coverage(t *testing.T) {
 	}
 
 	if omittedCount > 0 {
-		t.Fatal(fmt.Errorf("is expected that all items will be touched after first pass, but %d item(s) are untouched", omittedCount))
+		t.Fatal(fmt.Errorf("it is expected that all items will be touched after first pass, but %d items are untouched", omittedCount))
+	}
+}
+func TestUCB1Optimality(t *testing.T) {
+	itemCount := 50
+	statisticsList := make([]Statistics, 0, itemCount)
+	for i := 0; i < itemCount; i++ {
+		statisticsList = append(statisticsList, &BaseStatistics{})
+	}
+
+	alg, err := NewUCB1(statisticsList)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tryCount := itemCount * 5
+	for i := 0; i < tryCount; i++ {
+		index := alg.ResolveArmIndex()
+		if index < 0 {
+			t.Error(err)
+		}
+
+		statisticsList[index].SetCount(statisticsList[index].GetCount() + 1)
+		rand.Seed(time.Now().UnixNano())
+		if rand.Int31n(10) >= 5 {
+			statisticsList[index].SetReward(statisticsList[index].GetReward() + 1)
+		}
+	}
+
+	var totalCount int64
+
+	var maxCount int64
+	var maxCountIndex int
+
+	var maxReward float64
+	var maxRewardIndex int
+
+	for i := 0; i < itemCount; i++ {
+		count := statisticsList[i].GetCount()
+		if maxCount < count {
+			maxCount = count
+			maxCountIndex = i
+		}
+
+		reward := statisticsList[i].GetReward()
+		if maxReward < reward {
+			maxReward = reward
+			maxRewardIndex = i
+		}
+
+		totalCount += count
+	}
+
+	if totalCount != int64(tryCount) {
+		t.Errorf("it is expected that overall count will be equals to total tries count, but overall count is %d and total tries count is:%d\n", totalCount, tryCount)
+	}
+
+	if maxCountIndex != maxRewardIndex {
+		t.Errorf("it is expected that item with maximum reward will be chosen maximum times, but index of item with maximum reward is %d and index of item with maximum count is:%d\n", maxRewardIndex, maxCountIndex)
 	}
 }
