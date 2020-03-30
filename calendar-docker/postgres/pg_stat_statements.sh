db1bb48a9434ec901ee365878868fea47c344ac6
Grigoriy Zavodov <zavodov@gmail.com>
2020-03-30 22:37:08 +0200

    calendar-docker: monitoring

---
+++ calendar-docker/postgres/pg_stat_statements.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "shared_preload_libraries = 'pg_stat_statements'" >> $PGDATA/postgresql.conf
+echo "pg_stat_statements.max = 10000" >> $PGDATA/postgresql.conf
+echo "pg_stat_statements.track = all" >> $PGDATA/postgresql.conf
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE EXTENSION pg_stat_statements;
+    SELECT * FROM pg_available_extensions WHERE name = 'pg_stat_statements';
+EOSQL
